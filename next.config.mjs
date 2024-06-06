cb32fba4d815320e6fe6bdbd2d63545f27a40351
sokhenguchiha <sokhengchen@gmail.com>
2024-06-05 21:35:04 -0700

    feat: init supa-auth doc

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextra from "nextra";
 
-export default nextConfig;
+const nextConfig = {
+};
+
+const withNextra = nextra({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.jsx",
+});
+
+export default withNextra(nextConfig);
+
---
+++ theme.config.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line
+export default {
+	logo: <span>My Nextra Documentation</span>,
+	project: {
+		link: "https://github.com/shuding/nextra",
+	},
+	sidebar: {
+		titleComponent({ title, type }) {
+			if (type === "separator") {
+				return <div style={{ fontWeight: "bold" }}>{title}</div>;
+			}
+			// if (title === "About") {
+			// 	return <>❓ {title}</>;
+			// }
+			return <>{title}</>;
+		},
+	},
+};
